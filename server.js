28301c6f1b0a18844302797a5e2c02de88b37e7d
Andres Canales <andres.canales@macbook.com>
2017-09-26 14:00:53 -0600

    Initial commint with the integration of the theme and the product request to the api

---
+++ server.js
@@ -0,0 +1,71 @@
+/**
+ * Entry Point, and dependencies
+ */
+
+'use strict'
+ 
+const Hapi = require('hapi');
+const Request = require('request');
+const Vision = require('vision');
+const Handlebars = require('handlebars');
+const LodashFilter = require('lodash.filter');
+const LodashTake = require('lodash.take');
+ 
+const server = new Hapi.Server();
+ 
+server.connection({
+    host: '127.0.0.1',
+    port: 3001
+});
+ 
+// Register vision for our views
+server.register(Vision, (err) => {
+    server.views({
+        engines: {
+            html: Handlebars
+        },
+        relativeTo: __dirname,
+        path: './views',
+    });
+});
+ 
+server.start((err) => {
+    if (err) {
+        throw err;
+    }
+ 
+    console.log(`Server running at: ${server.info.uri}`);
+});
+
+// index
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        Request.get('http://localhost:3000/products', function (error, response, body) {
+            if (error) {
+                throw error;
+            }
+ 
+            const data = JSON.parse(body);
+            console.error(data[0].name);
+            reply.view('index', { result: data });
+        });
+    }
+});
+ 
+// Handler helpers:
+
+Handlebars.registerHelper("showColumn", function(index,block) {
+
+    if(parseInt(index)%3 === 0){
+        return block.fn(this);
+    }
+});
+Handlebars.registerHelper("closeColumn", function(index,block) {
+    if(parseInt(index) != 0){
+        if(parseInt(index)%2 === 0){
+            return block.fn(this);
+        }
+    }
+});
